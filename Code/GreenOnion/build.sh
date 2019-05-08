aa3e268ad0effc6a9f96c0b3afd268637ef55aa7
AidanFray <afray@hotmail.co.uk>
2019-05-08 15:43:38 +0100

    Changes; - Updated the build.sh script

--- Code/GreenOnion/build.sh
+++ Code/GreenOnion/build.sh
@@ -1,8 +1,8 @@
-g++ hash.cpp ./Crypto/sha1.cpp -pthread -lOpenCL -lcrypto -lssl
+g++ hash.cpp ./Crypto/sha1.cpp -pthread -lOpenCL -lcrypto -lssl -o GreenOnion.out
 rc=$?; 
 if [[ $rc != 0 ]]; 
 then 
     echo "[*] Error in complilation!"
     exit $rc;
 fi
-./a.out
+./GreenOnion.out
